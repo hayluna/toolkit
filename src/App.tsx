82347f47735074e1277921998df9d4611ee9d612
명혜은 <hayluna2@gmail.com>
2022-08-10 18:15:00 +0900

    임시 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,93 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import qs, { stringify } from 'qs'
+import { difference, isEqual, xor } from 'lodash'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [left, setLeft] = useState('')
+  const [right, setRight] = useState('')
+  const [leftParams, setLeftParams] = useState<{ key: string; value: any }[]>()
+  const [rightParams, setRightParams] = useState<{ key: string; value: any }[]>()
+  const [table, setTable] = useState<{ key: string; value1: any; value2: any }[]>([])
+
+  useEffect(() => {
+    if (left && right) {
+      const _table = []
+      const leftObj = qs.parse(left.split('?')[1], { ignoreQueryPrefix: true })
+      const rightObj = qs.parse(right.split('?')[1], { ignoreQueryPrefix: true })
+      console.log(leftObj, rightObj)
+      const keySet = new Set<string>()
+      Object.keys(leftObj).forEach((key) => {
+        keySet.add(key)
+      })
+      Object.keys(rightObj).forEach((key) => {
+        keySet.add(key)
+      })
+      const table = [...keySet.values()].map((key) => {
+        const row: { value1: any; value2: any } | any = {}
+        if (leftObj[key]) {
+          row.value1 = leftObj[key]
+        }
+        if (rightObj[key]) {
+          row.value2 = rightObj[key]
+        }
+        return { key, ...row }
+      })
+      setTable(table)
+      // setLeftParams(lp)
+      // setRightParams(rp)
+    }
+  }, [left, right])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="top">
+        <div>
+          <textarea value={left} onChange={(e) => setLeft(e.target.value)} rows={30} cols={30} />
+        </div>
+        <div>
+          <textarea value={right} onChange={(e) => setRight(e.target.value)} rows={30} cols={30} />
+        </div>
+      </div>
+      <div className="bottom">
+        <table>
+          <tbody>
+            <tr>
+              <td>key</td>
+              <td width="30%">value1</td>
+              <td width="30%">value2</td>
+              <td width="30%">diff</td>
+            </tr>
+            {table.map(({ key, value1, value2 }) => {
+              const diff = difference([value1], [value2])[0]
+              const isMissing = !value1 || !value2
+
+              return (
+                <tr key={key}>
+                  <td style={{ background: isMissing ? '#ed73a2' : 'transparent' }}>{key}</td>
+                  <td width="30%">
+                    {!value1
+                      ? ''
+                      : typeof value1 === 'object'
+                      ? JSON.stringify(value1, null, 2)
+                      : `${value1}`}
+                  </td>
+                  <td width="30%">
+                    {!value2
+                      ? ''
+                      : typeof value2 === 'object'
+                      ? JSON.stringify(value2, null, 2)
+                      : `${value2}`}
+                  </td>
+                  <td width="30%">
+                    {diff === null ? '' : typeof diff === 'object' ? JSON.stringify(diff) : diff}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
